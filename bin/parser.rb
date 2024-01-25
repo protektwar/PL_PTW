c7d164a120f16267f2d78b4a5d36ee1a5f2f21e6
Alexandru Herlas <alexandru.herlas@protektwar>
2024-01-25 15:11:28 +0100

    changed the module name

--- bin/parser.rb
+++ bin/parser.rb
@@ -1,16 +1,12 @@
 
 require_relative "../libs/SyntaxAnalyzer.rb"
 
-include RubyScript2CAPL
+include RubyScript
 
-$rubyScript2CAPL_running = true
+$rubyScript_running = true
 sourceFile = "bin/test.rcpl"
 
-#$lexycalAnalyzer = RubyScript2CAPL::LexicalAnalyzer.new()
-#$lexycalAnalyzer.lexycalChecker(file2Convert)
-
-$syntax = RubyScript2CAPL::SyntaxAnalyzer.new(sourceFile)
+$syntax = RubyScriptL::SyntaxAnalyzer.new(sourceFile)
 $syntax.setDebugLevel(0)
 $syntax.checker()
-#convertor.writeCAPLFile
 
